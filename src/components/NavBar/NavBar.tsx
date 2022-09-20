c56caa39ac9272ad8da52c914418e5768a3a1448
hahs <alexandre_2420@outlook.com>
2022-09-20 11:55:47 -0500

    initial setup project

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,18 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import React from "react";
+
+export interface NavBarInterface {}
+
+const NavBar: React.FC<NavBarInterface> = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          React PG
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
